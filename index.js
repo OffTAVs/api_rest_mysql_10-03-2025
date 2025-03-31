6871d12f404d78b29ea36ceb666fa981cab05a00
Otávio Barbosa Ferreira <106000641+OffTAVs@users.noreply.github.com>
2025-03-31 23:52:01 +0000

    teste

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const bodyParser = require('body-parser');
+const dotenv = require("dotenv")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-
-const connection = mysql.createConnection({
+let connection;
+const InitDB = async ( ) =>{
+const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD
   });
 
   
+   
+
+
     // Montando a tabela
-  connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`, (err) => {
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`, (err) => {
     if (err) throw err;
     console.log(`Banco de dados '${process.env.DB_NAME}' garantido.`);
   
@@ -39,30 +43,30 @@ const connection = mysql.createConnection({
     });
   });
 
-  
+}
 
   // Criar usuário
-app.post('/users', (req, res) => {
+app.post('/users', async(req, res) => {
     const { name, email } = req.body;
     const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
-    connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
+    await connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
       if (err) return res.status(500).json({ error: err.message });
       res.json({ message: 'Usuário criado com sucesso', id: results.insertId });
     });
   });
   
   // Obter todos os usuários
-  app.get('/users', (req, res) => {
-    connection.query('SELECT * FROM users', (err, results) => {
+  app.get('/users', async(req, res) => {
+    await connection.query('SELECT * FROM users', (err, results) => {
       if (err) return res.status(500).json({ error: err.message });
       res.json(results);
     });
   });
 
   // Obter um Usuário por ID
-  app.get('/users/:id', (req, res) =>{
+  app.get('/users/:id',async (req, res) =>{
 const {id} = req.params
-connection.query('SELECT * FROM users WHERE id = ?', [id],
+await connection.query('SELECT * FROM users WHERE id = ?', [id],
   (err, results) => {
     if (err) return res.status(500).json({erro:err.message});
     if (results.length ===0){
@@ -73,11 +77,11 @@ connection.query('SELECT * FROM users WHERE id = ?', [id],
 });
   
 //Atualizar usuário
-app.get('/users/:id', (req, res) =>{
+app.get('/users/:id',async (req, res) =>{
   const {id} = req.params
   const{name, email} = req.body;
   const UPDATE_USER_QUERY = 'UPDATE users SET name = ?, email = ? WHERE id = ?'
-  connection.query(UPDATE_USER_QUERY, [name,email,id], (err, results) => {
+  await connection.query(UPDATE_USER_QUERY, [name,email,id], (err, results) => {
     if (err) return res.status(500).json ({erro: err.message});
     if (results.affectedRows === 0){
       return res.status(404).json({error: 'Usuário não encontrado'});
@@ -89,10 +93,10 @@ app.get('/users/:id', (req, res) =>{
 
 
 //Deletar usuário
-app.delete('/users/:id', (req, res) =>{
+app.delete('/users/:id',async (req, res) =>{
   const {id} = req.params;
   const DELETE_USERS_QUERY = 'DELETE FROM users WHERE id = ?';
-  connection.query(DELETE_USERS_QUERY, [id], (err, results) => {
+  await connection.query(DELETE_USERS_QUERY, [id], (err, results) => {
     if (err) return res.status(500).json({error: err.message});
     if (results.affectedRows ===0 ){
       return res.status(404).json ({ error: 'Usuário não encontrado'});
@@ -101,9 +105,13 @@ app.delete('/users/:id', (req, res) =>{
   });
 });
 
-
-  const server= app.listen(port, () => {
+let server;
+InitDB.then(( )=>{
+  
+  server= app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
   });
+
+})
 
   module.exports = {app, server, connection};
